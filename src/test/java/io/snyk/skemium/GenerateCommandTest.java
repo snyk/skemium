7faac0658fd6958b0ca37b17b3c8632daf80e8e4
Ivan De Marino <ivan.demarino@snyk.io>
2025-06-02 10:01:12 +0100

    feat: abstract lifecycle of Postgres test container in abstract class

--- src/test/java/io/snyk/skemium/GenerateCommandTest.java
+++ src/test/java/io/snyk/skemium/GenerateCommandTest.java
@@ -2,8 +2,9 @@ package io.snyk.skemium;
 
 import io.snyk.skemium.avro.TableAvroSchemas;
 import io.snyk.skemium.meta.MetadataFile;
-import org.junit.jupiter.api.*;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
 import picocli.CommandLine;
 
@@ -17,20 +18,8 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class GenerateCommandTest {
-    static PostgreSQLContainer<?> POSTGRES_CONTAINER = TestHelper.initPostgresContainer();
-
+public class GenerateCommandTest extends WithPostgresContainer {
     Path TEMP_DIR;
-
-    @BeforeAll
-    static void startDB() {
-        POSTGRES_CONTAINER.start();
-    }
-
-    @AfterAll
-    static void stopDB() {
-        POSTGRES_CONTAINER.stop();
-    }
 
     @BeforeEach
     public void createTempDir() throws IOException {
@@ -54,10 +43,10 @@ public class GenerateCommandTest {
         // `generate` runs successfully
         assertEquals(0, cmdLine.execute(
                 "--hostname", POSTGRES_CONTAINER.getHost(),
-                "--port", POSTGRES_CONTAINER.getMappedPort(TestHelper.POSTGRES_DEFAULT_PORT).toString(),
-                "--database", TestHelper.DB_NAME,
-                "--username", TestHelper.DB_USER,
-                "--password", TestHelper.DB_PASS,
+                "--port", POSTGRES_CONTAINER.getMappedPort(POSTGRES_DEFAULT_PORT).toString(),
+                "--database", DB_NAME,
+                "--username", DB_USER,
+                "--password", DB_PASS,
                 TEMP_DIR.toAbsolutePath().toString()
         ));
 
@@ -67,10 +56,10 @@ public class GenerateCommandTest {
         // ... command line arguments
         assertEquals(List.of(
                 "--hostname", POSTGRES_CONTAINER.getHost(),
-                "--port", POSTGRES_CONTAINER.getMappedPort(TestHelper.POSTGRES_DEFAULT_PORT).toString(),
-                "--database", TestHelper.DB_NAME,
-                "--username", TestHelper.DB_USER,
-                "--password", TestHelper.DB_PASS,
+                "--port", POSTGRES_CONTAINER.getMappedPort(POSTGRES_DEFAULT_PORT).toString(),
+                "--database", DB_NAME,
+                "--username", DB_USER,
+                "--password", DB_PASS,
                 TEMP_DIR.toAbsolutePath().toString()), meta.arguments());
 
         // .. count of schemas
@@ -98,10 +87,10 @@ public class GenerateCommandTest {
         // `generate` runs successfully
         assertEquals(0, cmdLine.execute(
                 "--hostname", POSTGRES_CONTAINER.getHost(),
-                "--port", POSTGRES_CONTAINER.getMappedPort(TestHelper.POSTGRES_DEFAULT_PORT).toString(),
-                "--database", TestHelper.DB_NAME,
-                "--username", TestHelper.DB_USER,
-                "--password", TestHelper.DB_PASS,
+                "--port", POSTGRES_CONTAINER.getMappedPort(POSTGRES_DEFAULT_PORT).toString(),
+                "--database", DB_NAME,
+                "--username", DB_USER,
+                "--password", DB_PASS,
                 "--table", "employee",
                 "--table", "artist,album",
                 TEMP_DIR.toAbsolutePath().toString()
@@ -113,10 +102,10 @@ public class GenerateCommandTest {
         // ... command line arguments
         assertEquals(List.of(
                 "--hostname", POSTGRES_CONTAINER.getHost(),
-                "--port", POSTGRES_CONTAINER.getMappedPort(TestHelper.POSTGRES_DEFAULT_PORT).toString(),
-                "--database", TestHelper.DB_NAME,
-                "--username", TestHelper.DB_USER,
-                "--password", TestHelper.DB_PASS,
+                "--port", POSTGRES_CONTAINER.getMappedPort(POSTGRES_DEFAULT_PORT).toString(),
+                "--database", DB_NAME,
+                "--username", DB_USER,
+                "--password", DB_PASS,
                 "--table", "employee",
                 "--table", "artist,album",
                 TEMP_DIR.toAbsolutePath().toString()), meta.arguments());
@@ -146,10 +135,10 @@ public class GenerateCommandTest {
         // `generate` runs successfully
         assertEquals(0, cmdLine.execute(
                 "--hostname", POSTGRES_CONTAINER.getHost(),
-                "--port", POSTGRES_CONTAINER.getMappedPort(TestHelper.POSTGRES_DEFAULT_PORT).toString(),
-                "--database", TestHelper.DB_NAME,
-                "--username", TestHelper.DB_USER,
-                "--password", TestHelper.DB_PASS,
+                "--port", POSTGRES_CONTAINER.getMappedPort(POSTGRES_DEFAULT_PORT).toString(),
+                "--database", DB_NAME,
+                "--username", DB_USER,
+                "--password", DB_PASS,
                 "--table", "artist",
                 "-t", "album",
                 "-x", "public.artist.artist_id",
@@ -163,10 +152,10 @@ public class GenerateCommandTest {
         // ... command line arguments
         assertEquals(List.of(
                 "--hostname", POSTGRES_CONTAINER.getHost(),
-                "--port", POSTGRES_CONTAINER.getMappedPort(TestHelper.POSTGRES_DEFAULT_PORT).toString(),
-                "--database", TestHelper.DB_NAME,
-                "--username", TestHelper.DB_USER,
-                "--password", TestHelper.DB_PASS,
+                "--port", POSTGRES_CONTAINER.getMappedPort(POSTGRES_DEFAULT_PORT).toString(),
+                "--database", DB_NAME,
+                "--username", DB_USER,
+                "--password", DB_PASS,
                 "--table", "artist",
                 "-t", "album",
                 "-x", "public.artist.artist_id",
@@ -200,10 +189,10 @@ public class GenerateCommandTest {
         // `generate` runs successfully
         assertEquals(0, cmdLine.execute(
                 "--hostname", POSTGRES_CONTAINER.getHost(),
-                "--port", POSTGRES_CONTAINER.getMappedPort(TestHelper.POSTGRES_DEFAULT_PORT).toString(),
-                "--database", TestHelper.DB_NAME,
-                "--username", TestHelper.DB_USER,
-                "--password", TestHelper.DB_PASS,
+                "--port", POSTGRES_CONTAINER.getMappedPort(POSTGRES_DEFAULT_PORT).toString(),
+                "--database", DB_NAME,
+                "--username", DB_USER,
+                "--password", DB_PASS,
                 "--schema", "BaNaNa",
                 TEMP_DIR.toAbsolutePath().toString()
         ));
@@ -214,10 +203,10 @@ public class GenerateCommandTest {
         // ... command line arguments
         assertEquals(List.of(
                 "--hostname", POSTGRES_CONTAINER.getHost(),
-                "--port", POSTGRES_CONTAINER.getMappedPort(TestHelper.POSTGRES_DEFAULT_PORT).toString(),
-                "--database", TestHelper.DB_NAME,
-                "--username", TestHelper.DB_USER,
-                "--password", TestHelper.DB_PASS,
+                "--port", POSTGRES_CONTAINER.getMappedPort(POSTGRES_DEFAULT_PORT).toString(),
+                "--database", DB_NAME,
+                "--username", DB_USER,
+                "--password", DB_PASS,
                 "--schema", "BaNaNa",
                 TEMP_DIR.toAbsolutePath().toString()), meta.arguments());
 
@@ -238,10 +227,10 @@ public class GenerateCommandTest {
         // `generate` runs successfully
         assertEquals(1, cmdLine.execute(
                 "--hostname", POSTGRES_CONTAINER.getHost(),
-                "--port", String.valueOf(POSTGRES_CONTAINER.getMappedPort(TestHelper.POSTGRES_DEFAULT_PORT) + 1),
-                "--database", TestHelper.DB_NAME,
-                "--username", TestHelper.DB_USER,
-                "--password", TestHelper.DB_PASS,
+                "--port", String.valueOf(POSTGRES_CONTAINER.getMappedPort(POSTGRES_DEFAULT_PORT) + 1),
+                "--database", DB_NAME,
+                "--username", DB_USER,
+                "--password", DB_PASS,
                 TEMP_DIR.toAbsolutePath().toString()
         ));
     }
@@ -253,10 +242,10 @@ public class GenerateCommandTest {
         // `generate` runs successfully
         assertEquals(1, cmdLine.execute(
                 "--hostname", POSTGRES_CONTAINER.getHost(),
-                "--port", POSTGRES_CONTAINER.getMappedPort(TestHelper.POSTGRES_DEFAULT_PORT).toString(),
+                "--port", POSTGRES_CONTAINER.getMappedPort(POSTGRES_DEFAULT_PORT).toString(),
                 "--database", "BaNaNa",
-                "--username", TestHelper.DB_USER,
-                "--password", TestHelper.DB_PASS,
+                "--username", DB_USER,
+                "--password", DB_PASS,
                 TEMP_DIR.toAbsolutePath().toString()
         ));
     }
@@ -271,10 +260,10 @@ public class GenerateCommandTest {
         // `generate` runs successfully
         assertEquals(0, cmdLine.execute(
                 "--hostname", POSTGRES_CONTAINER.getHost(),
-                "--port", POSTGRES_CONTAINER.getMappedPort(TestHelper.POSTGRES_DEFAULT_PORT).toString(),
-                "--database", TestHelper.DB_NAME,
-                "--username", TestHelper.DB_USER,
-                "--password", TestHelper.DB_PASS,
+                "--port", POSTGRES_CONTAINER.getMappedPort(POSTGRES_DEFAULT_PORT).toString(),
+                "--database", DB_NAME,
+                "--username", DB_USER,
+                "--password", DB_PASS,
                 "--table", "playlist_track",
                 "--table", "playlist_track_no_pkey",
                 TEMP_DIR.toAbsolutePath().toString()
